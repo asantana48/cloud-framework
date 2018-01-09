5a62353c7816a0a8f90850e4ef094102a0dfc232
sjw256 <armorgamr@gmail.com>
2018-01-08 20:12:53 -0500

    Update SizePolicy.h

--- lib/SizePolicy.h
+++ lib/SizePolicy.h
@@ -1,12 +1,29 @@
 #ifndef SIZEPOLICY_H
 #define SIZEPOLICY_H
 
+#include "DataObject.h"
 #include "Policy.h"
+
 class SizePolicy : public Policy {
+private:
+	int threshold;
 public:
-    // TODO add logic
-    bool isFileKept() {
-        return true;
-    }
+	SizePolicy::SizePolicy(int newThreshold)
+	{
+		threshold = newThreshold;
+	}
+
+
+	bool isFileKept(FileData dataObject) 
+	{
+		if (dataObject.fileSize < threshold)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 };
 #endif
