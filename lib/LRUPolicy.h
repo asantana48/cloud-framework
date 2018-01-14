f9ac7366a4fc8f057fa2fb5a2854b833650ca72a
sjw256 <armorgamr@gmail.com>
2018-01-13 23:44:22 -0500

    Update LRUPolicy.h

--- lib/LRUPolicy.h
+++ lib/LRUPolicy.h
@@ -2,11 +2,30 @@
 #define LRUPOLICY_H
 
 #include "Policy.h"
+#include <ctime>
+
 class LRUPolicy: public Policy {
 public:
-    // TODO add logic
-    bool isFileKept(FileData dataObject) {
-        return false;
+
+	//necessary inputs: 
+	//dataObject
+	//minimum age of files to be migrated in seconds (i.e. move files older than minAge seconds)
+    bool isFileKept(FileData dataObject, long int minAge) {
+
+		long int currentTime = time(NULL);
+
+		//if last access is before current time minus minimum age of files to be migrated
+		if (dataObject.lastAccessed <= (currentTime - minAge))
+		{
+			//migrate the file
+			return false;
+		}
+		else
+		{
+			//keep the file
+			return true;
+		}
+
     }
     bool isFileKept(std::list<FileData> dataList) {
 
