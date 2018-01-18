a3df87eec9f63c220299dd47aebb968a002b0fa4
sjw256 <armorgamr@gmail.com>
2018-01-18 13:34:39 -0500

    Updated names and removed deletion on line 20

--- PolicyDriver.cpp
+++ PolicyDriver.cpp
@@ -1,24 +1,25 @@
-#include "lib/PolicyManager.h"
-#include "lib/SizePolicy.h"
-#include "lib/TimePolicy.h"
+#include "PolicyManager.h"
+#include "SizePolicy.h"
+#include "TimePolicy.h"
 
 #include <iostream>
 
 using namespace std;
 
-int main() {
-  PolicyManager pm;
-  SizePolicy* sp = new SizePolicy(5);
-  LRUPolicy* lp1 = new LRUPolicy();
-  LRUPolicy* lp2 = new LRUPolicy();
+int main()
+{
+	PolicyManager pm;
+	SizePolicy* sp = new SizePolicy(5, true);
+	TimePolicy* lp1 = new TimePolicy(500);
+	TimePolicy* lp2 = new TimePolicy(600);
 
-  pm.addPolicy(sp);
-  pm.addPolicy(lp1);
-  pm.addPolicy(lp2);
+	pm.addPolicy(sp);
+	pm.addPolicy(lp1);
+	pm.addPolicy(lp2);
 
-  delete sp, lp1, lp2;
+	//delete sp, lp1, lp2;
 
-  std::cout << pm.getListString() << std::endl;
+	std::cout << pm.getListString() << std::endl;
 
-  return 0;
+	return 0;
 }
