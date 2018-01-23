6b2e74c2e4a39effc34ca881c3ead9f7bd72c64a
sjw256 <armorgamr@gmail.com>
2018-01-23 17:00:58 -0500

    Added functionality.
    changed lastModified to time_t data type. Added functions to convert contents to strings.

--- lib/FileData.h
+++ lib/FileData.h
@@ -1,12 +1,32 @@
 #ifndef FILEDATA_H
 #define FILEDATA_H
 #include <string>
+#include <sstream>
+#include <ctime>
+
+using namespace std;
+
 struct FileData
 {
-  int fileSize;
-  int lastAccessed;
+	int fileSize;
+	time_t lastModified;
+
+
+	string getFileSizeAsString()
+	{
+		stringstream s;
+		s << fileSize;
+		return s.str();
+	}
+
+	string getLastModifiedAsString()
+	{
+		stringstream s;
+		s << lastModified;
+		return s.str();
+	}
+
+
 
 };
 #endif
-
-
