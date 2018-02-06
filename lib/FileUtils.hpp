da4ba8449e553c8a063483abc2bfe6ba3fccce21
Andres Santana <andres.santana048@outlook.com>
2018-02-05 19:03:17 -0500

    Added read and write functionality to FileUtils

--- lib/FileUtils.hpp
+++ lib/FileUtils.hpp
@@ -2,24 +2,40 @@
 #define FILEUTILS_HPP
 
 #include <cstdint>
+#include <cstdio>
 #include <string>
-#include <sys/stat.h>
+
+#include <io.h>   // replace with unistd for linux
+//#include <dirent.h>
+
 class FileUtils {
 public:
-    int Open(std::string resolvedPath, unsigned int oflags, int mode, int &handle) {
-
-    }
     int Read(int handle, char* data, int bytesToRead, int &bytesRead) {
-
+        bytesRead = 0;
+        while (bytesRead < bytesToRead) {
+            bytesRead += read(handle, data + bytesRead, bytesToRead - bytesRead);
+            if (bytesRead = -1) {
+                return bytesRead;
+            }
+        }
+        return 0;
     }
+
     int Write(int handle, char* data, int bytesToWrite, int &bytesWritten) {
-
+        bytesWritten = 0;
+        while (bytesWritten < bytesToWrite) {
+            bytesWritten += write(handle, data + bytesWritten, bytesToWrite);
+            if (bytesWritten = -1) {
+                return bytesWritten;
+            }
+        }
+        return 0;
     }
-    int Stat(std::string path, struct stat& buf) {
 
-    }
-    int Close(int ccurHandle, bool flush) {
-
-    }
+    /*int List(char* path, char* url, &pListDataOut,
+        &listDataSize,
+        attrFlag,
+        partialMatchingFlag,
+        addURLPrefix);*/
 };
 #endif
