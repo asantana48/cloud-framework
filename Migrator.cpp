e4400ccecac112b210b3338325d1925a86840716
asantana48 <andres.santana048@outlook.com>
2018-02-10 18:43:25 -0500

    continuing to add to migrator

--- Migrator.cpp
+++ Migrator.cpp
@@ -9,70 +9,13 @@
 #include <fcntl.h>
 #include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
 // Constants
 static const string FILES_PATH = "/home/andres/Projects/cloud-framework/testfiles/";        // directory
 static const string POLICY_PATH = "/home/andres/Projects/cloud-framework/res/policies.xml"; // file
-
-/*
- * processNode:
- * @reader: the xmlReader
- *
- * Dump information about the current node
- */
-static void processNode(xmlTextReaderPtr reader) {
-    const xmlChar *name, *value;
-
-    name = xmlTextReaderConstName(reader);
-    if (name == NULL)
-    name = BAD_CAST "--";
-
-    value = xmlTextReaderConstValue(reader);
-
-    printf("%d %d %s %d %d", 
-        xmlTextReaderDepth(reader),
-        xmlTextReaderNodeType(reader),
-        name,
-        xmlTextReaderIsEmptyElement(reader),
-        xmlTextReaderHasValue(reader));
-    if (value == NULL)
-    printf("\n");
-    else {
-        if (xmlStrlen(value) > 40)
-            printf(" %.40s...\n", value);
-        else
-        printf(" %s\n", value);
-    }
-}
-
-/**
- * streamFile:
- * @filename: the file name to parse
- *
- * Parse and print information about an XML file.
- */
-static void streamFile(const char *filename) {
-    xmlTextReaderPtr reader;
-    int ret;
-
-    reader = xmlReaderForFile(filename, NULL, 0);
-    if (reader != NULL) {
-        ret = xmlTextReaderRead(reader);
-        while (ret == 1) {
-            processNode(reader);
-            ret = xmlTextReaderRead(reader);
-        }
-        xmlFreeTextReader(reader);
-        if (ret != 0) {
-            fprintf(stderr, "%s : failed to parse\n", filename);
-        }
-    } else {
-        fprintf(stderr, "Unable to open %s\n", filename);
-    }
-}
-
 
 int main(int argc, char* argv[]) {
     AWSConnector aws;
@@ -90,17 +33,19 @@ int main(int argc, char* argv[]) {
         FileData fd;
         fd.location = full_path;
         fd.fileSize = statObj.st_size;
-
+        fd.lastModified = statObj.st_atime;
         files.push_back(fd);
     }
     
     for (FileData i: files) {
-        cout << i.location << " (" << i.fileSize << ")\n";
+        cout << i.location << " (" << i.fileSize << ")" << i.lastModified << "\n";
     }
     // Set policies
     PolicyManager pm;
     int MB12 = 12*1024*1024;
+    int atime = time(NULL) - 40000;
     pm.addPolicy(new SizePolicy(MB12, false));
+    pm.addPolicy(new TimePolicy(atime));
 
     return 0;
 }
