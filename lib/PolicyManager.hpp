3e4630663487390917582904c7fe9119f6562fc8
sjw256 <armorgamr@gmail.com>
2018-02-10 19:02:02 -0500

    updated to match policymanager.cpp

--- lib/PolicyManager.hpp
+++ lib/PolicyManager.hpp
@@ -10,9 +10,19 @@
 #include <string>
 #include <algorithm>
 
+#include <stdio.h>
+#include <libxml/xmlreader.h>
+#include <libxml/parser.h>
+#include <string>
+
 class PolicyManager {
 private:
     std::list<Policy*> policyList;
+	
+    void parseSizePolicy (xmlDocPtr doc, xmlNodePtr cur)
+    void parsePolicy (xmlDocPtr doc, xmlNodePtr cur)
+    static void streamFile(const char *filename)
+    
     
     // Out of 100
     int saturation;
@@ -24,6 +34,7 @@ public:
     void setMaxSaturation(int saturation);
 	bool isFilePromoted(FileData fd);
 	std::list<FileData> getFileDemotionList(std::list<FileData> fileList); 
+    void parsePoliciesFromXMLFile();
 
 };
 #endif
