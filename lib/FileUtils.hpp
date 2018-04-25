be56e531b8a6ff1b52bdc00aeed3541b9dae3d41
U-PSU-ERIE\ARS5964 <ARS5964@E8WLAB6691.psu-erie.bd.psu.edu>
2018-04-25 08:52:01 -0400

    Fixed include

--- lib/FileUtils.hpp
+++ lib/FileUtils.hpp
@@ -32,6 +32,7 @@
 #include <errno.h>
 #include <pthread.h>
 #include <signal.h>
+#include <list>
 
 // Uncomment to output debug trace messages to a log file instead of syslog
 #define OUTPUT_LOG_FILE  1
