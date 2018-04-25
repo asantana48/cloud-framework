eab744795e8134163068c16fcadfe44d151641c7
U-PSU-ERIE\ARS5964 <ARS5964@E8WLAB6691.psu-erie.bd.psu.edu>
2018-04-25 09:10:25 -0400

    Filled out test class

--- FullMigration_test.cpp
+++ FullMigration_test.cpp
@@ -1,7 +1,14 @@
 #include "lib/FileUtils.hpp"
 #include <iostream>
 
+using namespace std;
 int main(int argc, char* argv[]) {
 	FileUtils fUtil;
+	std::list<std::string> ls = fUtil.List("/ceph/content/", "");
+
+	cout << "Files in Aquari:\n";
+	for (std::string filename: ls){
+		cout << filename;
+	}
 	return 0;
 }
